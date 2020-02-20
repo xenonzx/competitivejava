cb5c667f08caacbf480cc15bcd352080a55c0985
ahmed eldakhakhny <ahmed0xenon@gmail.com>
2020-02-20 14:34:23 +0200

    template code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,28 @@
-package com.company;
+//package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
         //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
         //Scanner scanner = new Scanner(file);
         Scanner scanner = new Scanner(System.in);
-        int numberOfMagnets = scanner.nextInt();
-        String[] magnets = new String[numberOfMagnets];
-        for (int i =0; i<numberOfMagnets;i++){
-            magnets[i] = scanner.next();
-        }
-        int counter = 0;
+        String in = scanner.nextLine();
+        String sub = in.substring(1,in.length()-1);
+        String[] splited = sub.split(", ");
 
-        Stack<String> stack = new Stack<String>();
-        stack.push(magnets[0]);
-        // every pop counter ++
-        for (int i = 1; i<numberOfMagnets; i++){
-
-            if(magnets[i].equals( stack.peek())){
-                stack.push(magnets[i]);
-            }else{
-                stack.removeAllElements();
-                counter++;
-                stack.push(magnets[i]);
+        Set<Character> set = new HashSet<>();
+        for (int i = 0 ;i<splited.length;i++){
+            if (!splited[i].isEmpty()){
+                set.add(splited[i].charAt(0));
             }
         }
 
-        //if stack is not empty pop last and counter ++
-        if (!stack.empty()){
-            counter++;
-        }
-
-        System.out.println(counter);
-
-
+        System.out.print(set.size());
     }
 }
