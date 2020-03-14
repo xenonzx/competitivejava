39113035910d97f451cf7c8ffe8d5918d6ed597e
Ahmed EL-Dakhakhny <>
2020-03-14 15:19:26 +0200

    accepted solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 //package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Main {
@@ -11,18 +8,70 @@ public class Main {
 
         //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
         //Scanner scanner = new Scanner(file);
+
         Scanner scanner = new Scanner(System.in);
-        String in = scanner.nextLine();
-        String sub = in.substring(1,in.length()-1);
-        String[] splited = sub.split(", ");
+        int n = scanner.nextInt();
+        String s = scanner.next();
+        System.out.println(SvetaDecoded(s,n));
 
-        Set<Character> set = new HashSet<>();
-        for (int i = 0 ;i<splited.length;i++){
-            if (!splited[i].isEmpty()){
-                set.add(splited[i].charAt(0));
+    }
+
+    static String Sveta(String s){
+        // find mid
+        int l = s.length();
+        if (l == 1)
+            return  s;
+
+        int mid = (l%2 == 0)? l/2: (l+1)/2;
+        mid --;
+        // substing to mid
+        System.out.println("mid "+ mid );
+        return s.charAt( mid)+ Sveta( s.substring(0,mid)+s.substring(mid+1));
+    }
+    static String SvetaDecoded(String s, int length){
+        int i = 0;
+        int midIndex = (length%2==0)? (length-1)/2: length/2;
+        //midIndex --;
+        char[] out = new char[length];
+
+        // setting mid at index
+        out[midIndex] = s.charAt(0);
+        i++;
+
+        for (int j = 1; j <length ; ) {
+            if (length%2==1) {
+                char c = s.charAt(j);
+                int indexC = midIndex - i;
+                out[indexC] = c;
+                j++;
+
+                if (j==length){
+                    break;
+                }
+
+                char c2 = s.charAt(j);
+                int indexC2 = midIndex + i;
+                out[indexC2] = c2;
+                j++;
+
+            } else {
+
+                char c = s.charAt(j);
+                int indexC = midIndex + i;
+                out[indexC] = c;
+                j++;
+
+                if (j==length){
+                    break;
+                }
+                char c2 = s.charAt(j);
+                int indexC2 = midIndex - i;
+                out[indexC2] = c2;
+                j++;
             }
-        }
+            i++;
 
-        System.out.print(set.size());
+        }
+        return new String(out);
     }
 }
