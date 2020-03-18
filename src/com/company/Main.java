bad402d60a2c259e2304b0e7a860c615fb702bf8
Ahmed EL-Dakhakhny <>
2020-03-18 20:27:37 +0200

    time limit exceeded

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -12,17 +13,29 @@ public class Main {
         //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
         //Scanner scanner = new Scanner(file);
         Scanner scanner = new Scanner(System.in);
-        String in = scanner.nextLine();
-        String sub = in.substring(1,in.length()-1);
-        String[] splited = sub.split(", ");
-
-        Set<Character> set = new HashSet<>();
-        for (int i = 0 ;i<splited.length;i++){
-            if (!splited[i].isEmpty()){
-                set.add(splited[i].charAt(0));
-            }
+        String in = scanner.next();
+        BigInteger n =new BigInteger(in);
+        BigInteger counter = BigInteger.ZERO;
+        if (n.compareTo(new BigInteger("9"))>0){
+            counter = BigInteger.ONE;
         }
 
-        System.out.print(set.size());
+        BigInteger sum = sumDigits(n);
+        while (sum.compareTo(new BigInteger("9"))>0){
+            counter = counter.add(BigInteger.ONE);
+            sum =  sumDigits(sum);
+        }
+        System.out.println(counter);
     }
+     static BigInteger sumDigits(BigInteger n){
+        BigInteger divisor =  BigInteger.ONE;
+        BigInteger sum =  BigInteger.ZERO;
+
+        while ((n.divide(divisor).compareTo(BigInteger.ZERO))>0) {
+            sum = sum.add(n.mod(BigInteger.TEN));
+            n = n.divide(BigInteger.TEN);
+        }
+        return sum;
+     }
+
 }
