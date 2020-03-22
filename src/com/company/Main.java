fc450c2c18bfdd201878c377915a96a998b31be3
Ahmed EL-Dakhakhny <>
2020-03-22 13:06:49 +0200

    accepted solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,18 +11,26 @@ public class Main {
 
         //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
         //Scanner scanner = new Scanner(file);
-        Scanner scanner = new Scanner(System.in);
-        String in = scanner.nextLine();
-        String sub = in.substring(1,in.length()-1);
-        String[] splited = sub.split(", ");
 
-        Set<Character> set = new HashSet<>();
-        for (int i = 0 ;i<splited.length;i++){
-            if (!splited[i].isEmpty()){
-                set.add(splited[i].charAt(0));
-            }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int l = scanner.nextInt();
+        ArrayList<Integer> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            points.add(scanner.nextInt());
         }
 
-        System.out.print(set.size());
+        points.sort(Integer::compareTo);
+        double distanceFromStartToFirstPoint =  points.get(0) - 0;
+        double distanceFromEndToLastPoint =  l-points.get(n-1) ;
+        // inter-distance is the distance between  points
+        double maxInterDistance = 0;
+        for (int i = 0; i <n-1 ; i++) {
+            maxInterDistance = Math.max(points.get(i+1)-points.get(i),maxInterDistance);
+        }
+        double minInterRadius = maxInterDistance/2.0;
+        double minRadius = Math.max(minInterRadius,Math.max(distanceFromEndToLastPoint,distanceFromStartToFirstPoint));
+        System.out.println(minRadius);
+
     }
 }
