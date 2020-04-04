cfa4b3ce9cc323f707f42446285af0ea1505273b
Ahmed EL-Dakhakhny <>
2020-04-04 21:47:33 +0200

    ce and re

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,74 @@
 //package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-
-public class Main {
+ class Main {
 
     public static void main(String[] args) {
 
         //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
         //Scanner scanner = new Scanner(file);
         Scanner scanner = new Scanner(System.in);
-        String in = scanner.nextLine();
-        String sub = in.substring(1,in.length()-1);
-        String[] splited = sub.split(", ");
+        int t = Integer.parseInt(scanner.next());
 
-        Set<Character> set = new HashSet<>();
-        for (int i = 0 ;i<splited.length;i++){
-            if (!splited[i].isEmpty()){
-                set.add(splited[i].charAt(0));
+        for (int i = 0; i < t; i++) {
+            //read input
+            int n = Integer.parseInt(scanner.next());
+            int[][] matrix = new int[n][n];
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < n; col++) {
+                    matrix[row][col] = Integer.parseInt(scanner.next());
+                }
             }
+
+            int k = calculateTrace(matrix,n);
+            int r = rowsWithRepeatedElements(matrix,n);
+            int c = colsWithRepeatedElements(matrix,n);
+            int x = i+1;
+            System.out.println("Case #"+x+": "+k+" "+r+" "+c);
         }
 
-        System.out.print(set.size());
+    }
+
+    static int calculateTrace(int[][]matrix, int dimen){
+        int trace = 0;
+        for (int i = 0; i <dimen ; i++) {
+                trace += matrix[i][i];
+        }
+        return trace;
+    }
+    static int colsWithRepeatedElements(int[][]matrix, int dimen){
+        int colsCount = 0;
+        for (int j = 0; j <dimen ; j++) {
+            HashMap<Integer,Integer> hash=  new HashMap<>();
+            for (int i = 0; i < dimen; i++) {
+                Integer currentV = hash.get(matrix[i][j]);
+                if (currentV != null && currentV > 0){
+                    colsCount++;
+                    break;
+                }else {
+                    hash.put(matrix[i][j],1);
+                }
+            }
+
+        }
+        return colsCount;
+    }
+
+    static int rowsWithRepeatedElements(int[][]matrix, int dimen){
+        int rowsCount = 0;
+        for (int i = 0; i <dimen ; i++) {
+            HashMap<Integer,Integer> hash=  new HashMap<>();
+            for (int j = 0; j < dimen; j++) {
+                Integer currentV = hash.get(matrix[i][j]);
+                if (currentV != null && currentV > 0){
+                    rowsCount++;
+                    break;
+                }else {
+                    hash.put(matrix[i][j],1);
+                }
+            }
+
+        }
+        return rowsCount;
     }
 }
