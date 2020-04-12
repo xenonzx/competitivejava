d79cd27fedda7cd31a6701ed166e1070865c8d9d
Ahmed EL-Dakhakhny <>
2020-04-12 20:32:57 +0200

    time limit exceeded

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,22 +8,44 @@ import java.io.*;
 
  class Main {
 
-    public static void main(String[] args) {
-//
-//        //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
-//        //Scanner scanner = new Scanner(file);
-//        Scanner scanner = new Scanner(System.in);
-//        String in = scanner.nextLine();
-//        String sub = in.substring(1,in.length()-1);
-//        String[] splited = sub.split(", ");
-//
-//        Set<Character> set = new HashSet<>();
-//        for (int i = 0 ;i<splited.length;i++){
-//            if (!splited[i].isEmpty()){
-//                set.add(splited[i].charAt(0));
-//            }
-//        }
+     public static void main(String[] args) {
+         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+         int n = in.nextInt();
+         ArrayList<Integer> v =  new ArrayList<>();
+         ArrayList<Integer> u =  new ArrayList<>();
 
-        System.out.print("helloworld");
-    }
-}
+         for (int i = 0; i < n ; i++) {
+             int value = in.nextInt();
+             v.add(value);
+             u.add(value);
+         }
+         // sort is modified merge sort with O(nlogn)
+         u.sort(Integer::compareTo);
+
+         int m = in.nextInt();
+         for (int i = 0; i <m ; i++) {
+             int type = in.nextInt();
+             int l = in.nextInt();
+             int r = in.nextInt();
+             long sum = 0;
+             if (type == 1){
+                 // r inclusive
+                 sum = findSumValues(v,l-1,r );
+             }else {
+                 sum = findSumValues(u,l-1,r );
+             }
+             System.out.println(sum);
+
+         }
+
+     }
+
+     static long findSumValues(ArrayList<Integer> v, int from, int to){
+         long sum = 0;
+         for (int i = from; i <to ; i++) {
+             sum += v.get(i);
+         }
+         return  sum;
+     }
+
+ }
