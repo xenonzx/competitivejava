1b104a93f1b603d939dc30002dee4e6cf3aa819e
Ahmed EL-Dakhakhny <>
2020-04-12 19:37:24 +0200

    solution is not correct

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,65 +1,100 @@
+
+
+import javafx.util.Pair;
+
 import java.util.*;
 import java.io.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
-        for (int i = 1; i <= t; ++i) {
-            String s = in.next();
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j <s.length() ; j++) {
-                char c = s.charAt(j);
-                int digit = Integer.parseInt(""+c);
-                String prefixOpening = prefixOpening(digit);
-                String postfixClosing = postfixClosing(digit);
-                sb.append(prefixOpening);
-                sb.append(c);
-                sb.append(postfixClosing);
+        int tCases = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
+        for (int t = 1; t <= tCases; ++t) {
+            int R = in.nextInt();
+            int C = in.nextInt();
+            int[][] floor = new int[R][C];
+
+            for (int i = 0; i <R ; i++) {
+                for (int j = 0; j <C ; j++) {
+                    floor[i][j] = in.nextInt();
+                }
             }
-            String padded = sb.toString();
-            String minimised = minimizeParanthasis(padded);
-            System.out.println("Case #" + i + ": " + minimised);
-        }
-    }
-    static String prefixOpening(int d){
-        StringBuilder sb = new StringBuilder();
+            // competitionScore is the sum of all rounds
+            int competitionScore = 0;
+            boolean canContinueRound = true;
+            // while can still make rounds
+            while (canContinueRound) {
+                ArrayList<Pair<Integer, Integer>> pairsToBeRemoved = new ArrayList<>();
+                int roundScore = 0;
+                for (int i = 0; i < R; i++) {
+                    for (int j = 0; j < C; j++) {
+                        if (floor[i][j]==0){
+                            break;
+                        }
+                        roundScore += floor[i][j];
+                        int north = findNorthCN(floor, i, j - 1, R, C);
+                        int south = findSouthCN(floor, i, j + 1, R, C);
+                        int east = findEastCN(floor, i + 1, j, R, C);
+                        int west = findWestCN(floor, i - 1, j, R, C);
+                        double count = ((north > 0) ? 1 : 0) + ((south > 0) ? 1 : 0) + ((east > 0) ? 1 : 0) + ((west > 0) ? 1 : 0) ;
+                        // TODO break if count is one
+                        double sum = north + south + east + west;
+                        double average = sum / count;
+                        // keep if skill is equal to average skill and up
+                        if (floor[i][j] < average &&!pairsToBeRemoved.contains(new Pair(i, j)) ) {
+                            // add to list of pairs to be removed
+                            pairsToBeRemoved.add(new Pair(i, j));
+                        }
+                    }
+                }
+                // calculate round score  and add it to competition score
+                competitionScore += roundScore;
+                // remove pairs from matrix
+                int numberOfEleminations = pairsToBeRemoved.size();
+                for (int i = 0; i < numberOfEleminations; i++) {
+                    Pair<Integer, Integer> p = pairsToBeRemoved.get(i);
+                    floor[p.getKey()][p.getValue()] = 0;
 
-        for (int i = 0; i < d; i++) {
-            sb.append('(');
-        }
-        return sb.toString();
-    }
+                }
+                pairsToBeRemoved.clear();
+                // if no one is eleminated then it was the final round
+                if (numberOfEleminations == 0) {
+                    canContinueRound = false;
+                }
 
-    static String postfixClosing(int d){
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < d; i++) {
-            sb.append(')');
-        }
-        return sb.toString();
-    }
-
-    static String minimizeParanthasis(String s){
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length() ; i++) {
-            char c =  s.charAt(i);
-            if (stack.empty()){
-                stack.push(c);
-                continue;
             }
-            if (stack.peek() ==')' && c == '(' ){
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
+
+            System.out.println("Case #" + t + ": " + competitionScore);
         }
-
-        StringBuilder sb = new StringBuilder();
-        stack.forEach(c -> {
-            sb.append(c);
-        });
-
-        String result =sb.toString();
-        return result;
     }
+    //CN Stands for compassNeibor
+    // return the element north to current position
+    static int findNorthCN(int[][] floor, int row, int col, int rowMax, int colMax){
+        // general validation
+
+        if (row < 0 ||row >= rowMax|| col < 0 ||col >= colMax)
+            return 0;
+        return (floor[row][col]>0) ? floor[row][col]: findNorthCN(floor, row, col - 1, rowMax, colMax);
+    }
+
+    static int findSouthCN(int[][]floor, int row, int col, int rowMax, int colMax){
+        // general validation
+        if (row < 0 ||row >= rowMax|| col < 0 ||col >= colMax)
+            return 0;
+        return (floor[row][col]>0) ? floor[row][col]: findSouthCN(floor, row, col + 1, rowMax, colMax);
+    }
+
+    static int findWestCN(int[][]floor, int row, int col, int rowMax, int colMax){
+        // general validation
+        if (row < 0 ||row >= rowMax|| col < 0 ||col >= colMax)
+            return 0;
+        return (floor[row][col]>0) ? floor[row][col]: findWestCN(floor, row - 1, col , rowMax, colMax);
+    }
+    // return the element north to current position
+    static int findEastCN(int[][]floor, int row, int col, int rowMax, int colMax){
+        // general validation
+        if (row < 0 ||row >= rowMax|| col < 0 ||col >= colMax)
+            return 0;
+        return (floor[row][col]>0) ? floor[row][col]: findEastCN(floor, row, col + 1, rowMax, colMax);
+    }
+
 }
