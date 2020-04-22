99993602398d4d3c6a86cafb7fbe30035c722fa3
Ahmed EL-Dakhakhny <>
2020-04-22 23:52:44 +0200

    accpted isa

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,65 +1,32 @@
+
 import java.util.*;
 import java.io.*;
 public class Solution {
     public static void main(String[] args) {
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
-        for (int i = 1; i <= t; ++i) {
-            String s = in.next();
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j <s.length() ; j++) {
-                char c = s.charAt(j);
-                int digit = Integer.parseInt(""+c);
-                String prefixOpening = prefixOpening(digit);
-                String postfixClosing = postfixClosing(digit);
-                sb.append(prefixOpening);
-                sb.append(c);
-                sb.append(postfixClosing);
-            }
-            String padded = sb.toString();
-            String minimised = minimizeParanthasis(padded);
-            System.out.println("Case #" + i + ": " + minimised);
-        }
-    }
-    static String prefixOpening(int d){
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < d; i++) {
-            sb.append('(');
-        }
-        return sb.toString();
-    }
-
-    static String postfixClosing(int d){
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < d; i++) {
-            sb.append(')');
-        }
-        return sb.toString();
-    }
-
-    static String minimizeParanthasis(String s){
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length() ; i++) {
-            char c =  s.charAt(i);
-            if (stack.empty()){
-                stack.push(c);
-                continue;
-            }
-            if (stack.peek() ==')' && c == '(' ){
-                stack.pop();
-            } else {
-                stack.push(c);
+        // states =>  created, waiting, running, terminated
+        ArrayList<Character> runningProcesses = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner in = new Scanner(br);
+        int t = in.nextInt();
+        int currentTime = -1;
+        // we dont have to read all the entries we are only interested in the entries before or equal to  t
+        while (t>=currentTime){
+            currentTime =in.nextInt();
+            Character processName = in.next().charAt(0);
+            String processState = in.next();
+            //  if the new entry is after t do not add it
+            if (t>=currentTime && processState.equals("running")){
+                runningProcesses.add(processName);
+            }else if(t>=currentTime){
+                runningProcesses.remove(processName);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        stack.forEach(c -> {
-            sb.append(c);
-        });
-
-        String result =sb.toString();
-        return result;
+        String result = "-1";
+        if (runningProcesses.size()==1){
+            result = ""+runningProcesses.get(0);
+        }
+        System.out.println(result);
     }
+
 }
