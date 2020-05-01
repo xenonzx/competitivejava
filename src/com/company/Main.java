58ba55adfbcfcb676ff8d7c194e98b415166369e
Ahmed EL-Dakhakhny <>
2020-05-01 23:32:13 +0200

    adding public to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@
 import java.util.*;
 import java.io.*;
 
- class Main {
+ public class Main {
 
     public static void main(String[] args) {
 //
@@ -14,15 +14,6 @@ import java.io.*;
 //        //Scanner scanner = new Scanner(file);
 //        Scanner scanner = new Scanner(System.in);
 //        String in = scanner.nextLine();
-//        String sub = in.substring(1,in.length()-1);
-//        String[] splited = sub.split(", ");
-//
-//        Set<Character> set = new HashSet<>();
-//        for (int i = 0 ;i<splited.length;i++){
-//            if (!splited[i].isEmpty()){
-//                set.add(splited[i].charAt(0));
-//            }
-//        }
 
         System.out.print("helloworld");
     }
