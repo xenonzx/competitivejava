f1763127efcf9ed9c35340cecd32e5e5e1680a21
Ahmed EL-Dakhakhny <>
2020-05-02 12:27:02 +0200

    accepted solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,46 @@ import java.io.*;
 //        //Scanner scanner = new Scanner(file);
 //        Scanner scanner = new Scanner(System.in);
 //        String in = scanner.nextLine();
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-        System.out.print("helloworld");
+
+        int t = in.nextInt();
+        for (int i = 1; i <=t ; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            char[] movements =in.next().toCharArray();
+            String result ="IMPOSSIBLE";
+            for (int j = 0; j < movements.length; j++) {
+
+                if(x==0&& y==0){
+                    result=""+0;
+                }
+                switch (movements[j]) {
+                    case 'N':
+                        y++;
+                        break;
+                    case 'S':
+                        y--;
+                        break;
+                    case 'W':
+                        x--;
+                        break;
+                    case 'E':
+                        x++;
+                        break;
+                }
+                int distance = Math.abs(x)+Math.abs(y);
+                if (distance <= j+1){
+                    int movmentNeeded =j+1;
+                    result ="" +movmentNeeded;
+                    break;
+                }
+
+            }
+
+            System.out.println("Case #" + i+ ": " + result);
+        }
+
+
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -3,63 +3,44 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
-        for (int i = 1; i <= t; ++i) {
-            String s = in.next();
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j <s.length() ; j++) {
-                char c = s.charAt(j);
-                int digit = Integer.parseInt(""+c);
-                String prefixOpening = prefixOpening(digit);
-                String postfixClosing = postfixClosing(digit);
-                sb.append(prefixOpening);
-                sb.append(c);
-                sb.append(postfixClosing);
+
+        int t = in.nextInt();
+        for (int i = 1; i <=t ; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            char[] movements =in.next().toCharArray();
+            String result ="IMPOSSIBLE";
+            for (int j = 0; j < movements.length; j++) {
+
+                if(x==0&& y==0){
+                    result=""+0;
+                }
+                switch (movements[j]) {
+                    case 'N':
+                        y++;
+                        break;
+                    case 'S':
+                        y--;
+                        break;
+                    case 'W':
+                        x--;
+                        break;
+                    case 'E':
+                        x++;
+                        break;
+                }
+                int distance = Math.abs(x)+Math.abs(y);
+                if (distance <= j+1){
+                    int movmentNeeded =j+1;
+                    result ="" +movmentNeeded;
+                    break;
+                }
+
             }
-            String padded = sb.toString();
-            String minimised = minimizeParanthasis(padded);
-            System.out.println("Case #" + i + ": " + minimised);
-        }
-    }
-    static String prefixOpening(int d){
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < d; i++) {
-            sb.append('(');
-        }
-        return sb.toString();
-    }
-
-    static String postfixClosing(int d){
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < d; i++) {
-            sb.append(')');
-        }
-        return sb.toString();
-    }
-
-    static String minimizeParanthasis(String s){
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length() ; i++) {
-            char c =  s.charAt(i);
-            if (stack.empty()){
-                stack.push(c);
-                continue;
-            }
-            if (stack.peek() ==')' && c == '(' ){
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
+            System.out.println("Case #" + i+ ": " + result);
         }
 
-        StringBuilder sb = new StringBuilder();
-        stack.forEach(c -> {
-            sb.append(c);
-        });
 
-        String result =sb.toString();
-        return result;
     }
 }
