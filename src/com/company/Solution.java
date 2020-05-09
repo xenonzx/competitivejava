693966b79be43d1117d91a8b2728135ff45a80bb
Ahmed EL-Dakhakhny <>
2020-05-09 16:39:37 +0200

    un acccepted

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -2,64 +2,84 @@ import java.util.*;
 import java.io.*;
 public class Solution {
     public static void main(String[] args) {
+//
+//        //File file = new File("/Users/ahmed/Development/CompetitiveProgramming\ git java/src/com/company/in.txt");
+//        //Scanner scanner = new Scanner(file);
+//        Scanner scanner = new Scanner(System.in);
+//        String in = scanner.nextLine();
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
-        for (int i = 1; i <= t; ++i) {
-            String s = in.next();
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j <s.length() ; j++) {
-                char c = s.charAt(j);
-                int digit = Integer.parseInt(""+c);
-                String prefixOpening = prefixOpening(digit);
-                String postfixClosing = postfixClosing(digit);
-                sb.append(prefixOpening);
-                sb.append(c);
-                sb.append(postfixClosing);
+
+
+        int t = in.nextInt();
+        for (int i = 1; i <=t ; i++) {
+            // slice size and its count
+            TreeMap<Long,Integer> slicesCounters = new TreeMap<>();
+            int n = in.nextInt();
+            int d = in.nextInt();
+            int chops = 0;
+            for (int j = 0; j < n; j++) {
+                Long sliceSize = in.nextLong();
+                int sliceCount  = slicesCounters.getOrDefault(sliceSize,0);
+                sliceCount++;
+                slicesCounters.put(sliceSize,sliceCount);
             }
-            String padded = sb.toString();
-            String minimised = minimizeParanthasis(padded);
-            System.out.println("Case #" + i + ": " + minimised);
-        }
-    }
-    static String prefixOpening(int d){
-        StringBuilder sb = new StringBuilder();
+            Map.Entry<Long, Integer> maxEntry = getlargestNumberOfSlicesEntry(slicesCounters);
+            while (maxEntry.getValue()<d){
+                chops++;
 
-        for (int i = 0; i < d; i++) {
-            sb.append('(');
-        }
-        return sb.toString();
-    }
 
-    static String postfixClosing(int d){
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < d; i++) {
-            sb.append(')');
-        }
-        return sb.toString();
-    }
+                //cut to increase the max
 
-    static String minimizeParanthasis(String s){
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length() ; i++) {
-            char c =  s.charAt(i);
-            if (stack.empty()){
-                stack.push(c);
-                continue;
+                //now we know which y
+                long y;
+                Iterator<Long> iterator = slicesCounters.descendingKeySet().descendingIterator();
+                Long sizeWithMaxCount = iterator.next();
+                if ( sizeWithMaxCount == maxEntry.getKey()){
+                    y = iterator.next();
+                } else {
+                    y = sizeWithMaxCount;
+                }
+                // cut the one that can give you 2 eqaul slices or the largest one
+                int entryToChoppedCounter = 0;
+                long entryToChoppedKey = 0;
+//                Map.Entry<Long, Integer> entryTobeChopped = null;
+//                for (Map.Entry<Long, Integer> entry : slicesCounters.entrySet())
+//                {
+//                    if ( entry.getValue()/2 == y) {
+//                        entryToChoppedKey = entry.getKey();
+//                        entryToChoppedCounter = entry.getValue();
+//                    }
+//                }
+                // chop
+                long slice1 = y;
+                long slice2 = sizeWithMaxCount - y;
+                int s1counter = slicesCounters.get(slice1)-1;
+                slicesCounters.put(slice1,slicesCounters.getOrDefault(slice1,0)+1);
+                slicesCounters.put(slice2,slicesCounters.getOrDefault(slice2,0)+1);
+                slicesCounters.put(sizeWithMaxCount,slicesCounters.getOrDefault(sizeWithMaxCount,0)-1);
+
+//                if(entryTobeChopped == null){
+//                    entryToChoppedKey = y;
+//                    entryToChoppedCounter = slicesCounters.get(y);
+//                }
+
             }
-            if (stack.peek() ==')' && c == '(' ){
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
+            System.out.println("Case #" + i+ ": " + chops);
         }
 
-        StringBuilder sb = new StringBuilder();
-        stack.forEach(c -> {
-            sb.append(c);
-        });
 
-        String result =sb.toString();
-        return result;
+    }
+
+    private static Map.Entry<Long, Integer> getlargestNumberOfSlicesEntry(TreeMap<Long, Integer> slicesCounters) {
+        Map.Entry<Long, Integer> maxEntry = null;
+        for (Map.Entry<Long, Integer> entry : slicesCounters.entrySet())
+        {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+            {
+                maxEntry = entry;
+            }
+        }
+        return maxEntry;
     }
 }
