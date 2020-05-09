e9d2a3c1d83ce280b307c7e21d3ec408f53b5b73
Ahmed EL-Dakhakhny <>
2020-05-09 17:12:05 +0200

    accpeted solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,78 +15,41 @@ import java.io.*;
 //        Scanner scanner = new Scanner(System.in);
 //        String in = scanner.nextLine();
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-
-
-        int t = in.nextInt();
-        for (int i = 1; i <=t ; i++) {
-            // slice size and its count
-            TreeMap<Long,Integer> slicesCounters = new TreeMap<>();
-            int n = in.nextInt();
-            int d = in.nextInt();
-            int chops = 0;
-            for (int j = 0; j < n; j++) {
-                Long sliceSize = in.nextLong();
-                int sliceCount  = slicesCounters.getOrDefault(sliceSize,0);
-                sliceCount++;
-                slicesCounters.put(sliceSize,sliceCount);
-            }
-            Map.Entry<Long, Integer> maxEntry = getlargestNumberOfSlicesEntry(slicesCounters);
-            while (maxEntry.getValue()<d){
-            chops++;
-
-
-
-                //cut to increase the max
-
-                //now we know which y
-                long y;
-                Iterator<Long> iterator = slicesCounters.descendingKeySet().descendingIterator();
-                Long sizeWithMaxCount = iterator.next();
-                if ( sizeWithMaxCount == maxEntry.getKey()){
-                    y = iterator.next();
-                } else {
-                    y = sizeWithMaxCount;
-                }
-                // cut the one that can give you 2 eqaul slices or the largest one
-                int entryToChoppedCounter = 0;
-                long entryToChoppedKey = 0;
-//                Map.Entry<Long, Integer> entryTobeChopped = null;
-//                for (Map.Entry<Long, Integer> entry : slicesCounters.entrySet())
-//                {
-//                    if ( entry.getValue()/2 == y) {
-//                        entryToChoppedKey = entry.getKey();
-//                        entryToChoppedCounter = entry.getValue();
-//                    }
-//                }
-                // chop
-                long slice1 = y;
-                long slice2 = sizeWithMaxCount - y;
-                int s1counter = slicesCounters.get(slice1)-1;
-                slicesCounters.put(slice1,slicesCounters.getOrDefault(slice1,0)+1);
-                slicesCounters.put(slice2,slicesCounters.getOrDefault(slice2,0)+1);
-                slicesCounters.put(sizeWithMaxCount,slicesCounters.getOrDefault(sizeWithMaxCount,0)-1);
-
-//                if(entryTobeChopped == null){
-//                    entryToChoppedKey = y;
-//                    entryToChoppedCounter = slicesCounters.get(y);
-//                }
-
-            }
-        System.out.println("Case #" + i+ ": " + chops);
+        int n = in.nextInt();
+        ArrayList<Integer>  nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nums.add(in.nextInt());
+        }
+        for (int i = 0; i < nums.size(); i++) {
+           System.out.println( calculateNumberOfNonZero(nums.get(i)));
+            System.out.println(intat(nums.get(i)));
         }
 
-
+    }
+    private static String intat(int integer){
+        int counter = 0 ;
+        String s = "";
+        while (integer>0){
+            if (integer % 10 !=0 ){
+                int roundInt = integer % 10 ;
+                int multiplier = (int) Math.pow(10,counter);
+                roundInt*=multiplier;
+                s= +roundInt+" "+s;
+            }
+            integer/=10;
+            counter++;
+        }
+        return s;
     }
 
-     private static Map.Entry<Long, Integer> getlargestNumberOfSlicesEntry(TreeMap<Long, Integer> slicesCounters) {
-         Map.Entry<Long, Integer> maxEntry = null;
-         for (Map.Entry<Long, Integer> entry : slicesCounters.entrySet())
-         {
-             if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
-             {
-                 maxEntry = entry;
-             }
-         }
-         return maxEntry;
+     private static int calculateNumberOfNonZero(Integer integer) {
+        int nonZeroElementsCount = 0 ;
+        while (integer>0){
+            if (integer % 10 !=0 ){
+                nonZeroElementsCount++;
+            }
+            integer/=10;
+        }
+        return nonZeroElementsCount;
      }
  }
